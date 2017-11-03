89853d582eee3fef1ee4452646fd25e7ac80e2fc
mortuzahossain <mortuzahossain1997@gmail.com>
2017-11-04 02:29:36 +0600

    Adding todo using function.

--- inc/functions.php
+++ inc/functions.php
@@ -1,15 +1,28 @@
 <?php
 include('db_config.php');
-// Inserting Task
+// Inserting Task From Index Page
 if (isset($_POST['task_save'])) {
+	add_task('index',$result);
+}
+
+function add_task($page,$result)
+{
+	global $con;
 	$task 		= $_POST['todo'];
 	$sql 		= "INSERT INTO todos (tasks,visibility) VALUES ('$task',1)";
 	$result     = mysqli_query($con,$sql);
+
 	if ($result) {
-		header('Location: ../index.php?message=taskadded');
-        exit();
+		$location = "Location: ../".$page.".php?message=taskadded";
+		redirect($location);
 	} else {
-		header('Location: ../index.php?message=error');
-        exit();
+		$location = "Location: ../".$page.".php?message=error";
+		redirect($location);
 	}
+}
+
+function redirect($location)
+{
+	header($location);
+	exit();
 }
