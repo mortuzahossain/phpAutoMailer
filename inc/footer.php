065a691ce258db7cb3166ee1ddb3a3a5dae8e0fe
mortuzahossain <mortuzahossain1997@gmail.com>
2017-11-03 20:14:50 +0600

    Index UI Prepare

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+		<div class="clearfix"> </div>
+		<div class="footer">
+			<p>© <?php date('Y') ?> All Rights Reserved . Design by <a href="http://www.facebook.com/mdmortuza.hossain/">Md Mortuza Hossain</a></p>
+		</div>
+	</section>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/proton.js"></script>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,229 @@
+
+
+<!DOCTYPE html>
+<head>
+	<title>PHP - AutoMailer || Mortuza Hossain</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/font.css" type="text/css"/>
+	<link href="css/font-awesome.css" rel="stylesheet"> 
+
+	<script src="js/jquery2.0.3.min.js"></script>
+	<script src="js/modernizr.js"></script>
+	<script src="js/jquery.cookie.js"></script>
+	<script src="js/screenfull.js"></script>
+	<script>
+	$(function () {
+		$('#supported').text('Supported/allowed: ' + !!screenfull.enabled);
+
+		if (!screenfull.enabled) {
+			return false;
+		}
+		$('#toggle').click(function () {
+			screenfull.toggle($('#container')[0]);
+		});	
+	});
+	</script>
+	<script src="js/raphael-min.js"></script>
+	<script src="js/morris.js"></script>
+	<link rel="stylesheet" href="css/morris.css">
+	<script src="js/skycons.js"></script>
+</head>
+<body class="dashboard-page">
+	<nav class="main-menu">
+		<ul>
+			<li>
+				<a href="index.html">
+					<i class="fa fa-home nav_icon"></i>
+					<span class="nav-text">
+					Dashboard
+					</span>
+				</a>
+			</li>
+			<li class="has-subnav">
+				<a href="javascript:;">
+				<i class="fa fa-cogs" aria-hidden="true"></i>
+				<span class="nav-text">
+					UI Components
+				</span>
+				<i class="icon-angle-right"></i><i class="icon-angle-down"></i>
+				</a>
+				<ul>
+					<li>
+					<a class="subnav-text" href="buttons.html">
+					Buttons
+					</a>
+					</li>
+					<li>
+					<a class="subnav-text" href="grids.html">
+					Grids
+					</a>
+					</li>
+				</ul>
+			</li>
+			<li class="has-subnav">
+				<a href="javascript:;">
+				<i class="fa fa-check-square-o nav_icon"></i>
+				<span class="nav-text">
+				Forms
+				</span>
+				<i class="icon-angle-right"></i><i class="icon-angle-down"></i>
+				</a>
+				<ul>
+					<li>
+						<a class="subnav-text" href="inputs.html">Inputs</a>
+					</li>
+					<li>
+						<a class="subnav-text" href="validation.html">Form Validation</a>
+					</li>
+				</ul>
+			</li>
+			<li class="has-subnav">
+				<a href="javascript:;">
+					<i class="fa fa-file-text-o nav_icon"></i>
+						<span class="nav-text">Pages</span>
+					<i class="icon-angle-right"></i><i class="icon-angle-down"></i>
+				</a>
+				<ul>
+					<li>
+						<a class="subnav-text" href="gallery.html">
+							Image Gallery
+						</a>
+					</li>
+					<li>
+						<a class="subnav-text" href="calendar.html">
+							Calendar
+						</a>
+					</li>
+					<li>
+						<a class="subnav-text" href="signup.html">
+							Sign Up Page
+						</a>
+					</li>
+					<li>
+						<a class="subnav-text" href="login.html">
+							Login Page
+						</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="charts.html">
+					<i class="fa fa-bar-chart nav_icon"></i>
+					<span class="nav-text">
+						Charts
+					</span>
+				</a>
+			</li>
+			<li>
+				<a href="typography.html">
+					<i class="icon-font nav-icon"></i>
+					<span class="nav-text">
+					Typography
+					</span>
+				</a>
+			</li>
+			<li>
+				<a href="tables.html">
+					<i class="icon-table nav-icon"></i>
+					<span class="nav-text">
+					Tables
+					</span>
+				</a>
+			</li>
+			<li>
+				<a href="maps.html">
+					<i class="fa fa-map-marker" aria-hidden="true"></i>
+					<span class="nav-text">
+					Maps
+					</span>
+				</a>
+			</li>
+			<li>
+				<a href="error.html">
+					<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+					<span class="nav-text">
+					Error Page
+					</span>
+				</a>
+			</li>
+			<li class="has-subnav">
+				<a href="javascript:;">
+					<i class="fa fa-list-ul" aria-hidden="true"></i>
+					<span class="nav-text">Extras</span>
+					<i class="icon-angle-right"></i><i class="icon-angle-down"></i>
+				</a>
+				<ul>
+					<li>
+						<a class="subnav-text" href="faq.html">FAQ</a>
+					</li>
+					<li>
+						<a class="subnav-text" href="blank.html">Blank Page</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+		<ul class="logout">
+			<li>
+			<a href="login.html">
+			<i class="icon-off nav-icon"></i>
+			<span class="nav-text">
+			Logout
+			</span>
+			</a>
+			</li>
+		</ul>
+	</nav>
+
+	<section class="wrapper scrollable">
+		<nav class="user-menu">
+			<a href="javascript:;" class="main-menu-access">
+			<i class="icon-proton-logo"></i>
+			<i class="icon-reorder"></i>
+			</a>
+		</nav>
+		<section class="title-bar">
+			<div class="logo">
+				<h1><a href="index.php"><img src="images/logo.png" alt="" />AutoMailer</a></h1>
+			</div>
+			<div class="full-screen">
+				<section class="full-top">
+					<button id="toggle"><i class="fa fa-arrows-alt" aria-hidden="true"></i></button>	
+				</section>
+			</div>
+			<div class="w3l_search">
+				<form action="" method="post">
+					<input type="text" name="search" value="Search" required="1">
+					<button class="btn btn-default" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+				</form>
+			</div>
+			<div class="header-right">
+				<div class="profile_details_left">
+					<div class="header-right-left">
+						<ul><p class="user-name">Mortuza Hossain</p></ul>
+					</div>
+					<div class="profile_details">		
+						<ul>
+							<li class="dropdown profile_details_drop">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+									<div class="profile_img">	
+										<span class="prfil-img"><i class="fa fa-user" aria-hidden="true"></i></span> 
+										<div class="clearfix"></div>	
+									</div>	
+								</a>
+								<ul class="dropdown-menu drp-mnu">
+									<li> <a href="#"><i class="fa fa-cog"></i> Settings</a> </li> 
+									<li> <a href="#"><i class="fa fa-user"></i> Profile</a> </li> 
+									<li> <a href="#"><i class="fa fa-sign-out"></i> Logout</a> </li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+		</section>
